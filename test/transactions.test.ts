39ab86435a687223dfe7a4e0fb59fb19a3bfce0f
Giuliano Crivelli <giuliano.crivelli@byside.com>
2023-03-19 03:48:32 -0400

    test: removing and running migrations every test

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,4 +1,5 @@
-import { describe, it, beforeAll, afterAll, expect } from 'vitest'
+import { execSync } from 'node:child_process'
+import { describe, it, beforeAll, afterAll, expect, beforeEach } from 'vitest'
 import request from 'supertest'
 
 import { app } from '../src/app'
@@ -10,6 +11,11 @@ describe('Transactions routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
